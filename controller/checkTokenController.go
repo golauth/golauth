ccc09dcdf6be68471cba69a0b36562910e145b49
Rafael Ramos <arkanjo.ms@gmail.com>
2020-04-11 02:52:56 -0300

    Develop (#2)
    * Improving response token.
    
    * Including cors config
    implementing security and public paths
    Including resource /users/{username}
    Refactoring token validation

--- controller/checkTokenController.go
+++ controller/checkTokenController.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"encoding/json"
-	"github.com/dgrijalva/jwt-go"
 	"golauth/model"
 	"golauth/util"
 	"net/http"
@@ -11,35 +9,15 @@ import (
 type CheckTokenController struct{}
 
 func (c CheckTokenController) CheckToken(w http.ResponseWriter, r *http.Request) {
-	authorization := r.Header.Get("Authorization")
-	token := authorization[7:]
-
-	claims := &Claims{}
-	parsedToken, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (i interface{}, err error) {
-		return verifyKey, nil
-	})
-
-	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			sendError(w, http.StatusUnauthorized, err.Error())
-			return
-		}
-		sendError(w, http.StatusBadRequest, err.Error())
+	token, err := util.ExtractToken(r)
+	if err == (model.Error{}) {
+		util.SendError(w, err)
 		return
 	}
-
-	if !parsedToken.Valid {
-		sendError(w, http.StatusUnauthorized, "Invalid token.")
+	err = util.ValidateToken(token)
+	if err == (model.Error{}) {
+		util.SendError(w, err)
 		return
 	}
-
 	util.SendSuccess(w, true)
-}
-
-func sendError(w http.ResponseWriter, statusCode int, message string) {
-	var e model.Error
-	e.Message = message
-	e.StatusCode = statusCode
-	w.WriteHeader(http.StatusUnauthorized)
-	_ = json.NewEncoder(w).Encode(e)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang-migrate/migrate/v4 v4.8.0
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/subosito/gotenv v1.2.0
