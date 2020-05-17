d3b35217faddfe3a7ec0b8e386073763dfa6296a
Rafael Ramos <arkanjo.ms@gmail.com>
2020-05-17 18:54:25 -0300

    Adding document field to user model

--- migrations/20200218235057_initial_data.up.sql
+++ migrations/20200218235057_initial_data.up.sql
@@ -13,9 +13,12 @@ select (select r.id from golauth_role r where r.name = 'ADMIN'), (select a.id fr
 insert into golauth_role_authority (role_id, authority_id)
 select (select r.id from golauth_role r where r.name = 'USER'), (select a.id from golauth_authority a where a.name = 'USER');
 
-insert into golauth_user (username, first_name, last_name, email, password)
-values ('admin', 'Admin', 'Admin', 'admin@goauth.org',
+insert into golauth_user (username, first_name, last_name, email, document, password)
+values ('admin', 'Admin', 'Admin', 'admin@goauth.org', '000',
         '$2a$10$VNkiJ40.00IfVjxo8ILyauLUbnxMcKK2G/FbbwdsTYb.lCuZEbh22'); -- password = admin123
 
 insert into golauth_user_role (role_id, user_id)
 select (select r.id from golauth_role r where r.name = 'ADMIN'), (select u.id from golauth_user u where u.username = 'admin');
+
+insert into golauth_user_role (role_id, user_id)
+select (select r.id from golauth_role r where r.name = 'USER'), (select u.id from golauth_user u where u.username = 'admin');
--- repository/userRepository.go
+++ repository/userRepository.go
@@ -19,7 +19,7 @@ func (u UserRepository) FindByUsernameWithPassword(username string) (model.User,
 func (u UserRepository) findByUsername(username string, omitPassword bool) (model.User, error) {
 	user := model.User{}
 	row := db.GetDatasource().QueryRow("SELECT * FROM golauth_user WHERE username = $1", username)
-	err := row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.Enabled, &user.CreationDate)
+	err := row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.Document, &user.Password, &user.Enabled, &user.CreationDate)
 	if omitPassword {
 		user.Password = ""
 	}
@@ -30,12 +30,12 @@ func (u UserRepository) FindByID(id int) (interface{}, error) {
 	user := model.User{}
 	var phantonZone string
 	row := db.GetDatasource().QueryRow("SELECT * FROM golauth_user WHERE id = $1", id)
-	err := row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &phantonZone, &user.Enabled, &user.CreationDate)
+	err := row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.Document, &phantonZone, &user.Enabled, &user.CreationDate)
 	return util.ResultData(user, model.User{}, err)
 }
 
 func (u UserRepository) Create(user model.User) (model.User, error) {
-	err := db.GetDatasource().QueryRow("INSERT INTO golauth_user (username, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id;",
-		user.Username, user.FirstName, user.LastName, user.Email, user.Password).Scan(&user.ID)
+	err := db.GetDatasource().QueryRow("INSERT INTO golauth_user (username, first_name, last_name, email, document, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
+		user.Username, user.FirstName, user.LastName, user.Email, user.Document, user.Password).Scan(&user.ID)
 	return user, err
 }
